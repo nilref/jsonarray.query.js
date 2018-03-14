ca689311f69dda552c41ea33c5e9026611d58f23
404 Not Found <mahatmasmile@qq.com>
2018-03-14 13:09:28 +0800

    新增数组操作函数
    1、根据条件删除指定元素；
    2、获取第一个满足查询条件元素下标；

--- JSONArray.Query.js
+++ JSONArray.Query.js
@@ -1,6 +1,10 @@
 //JSON数组 查询插件
 /*
-* JSONArray.Query('@colname="val"') return [...];
+* JSONArray.Query('@colname1="val1" OR @colname2=val2') return [{...},{...},...];
+* JSONArray.IndexOf('@colname1="val1" OR @colname2=val2') return -1;
+* JSONArray.Remove('@colname1="val1" OR @colname2=val2') return [{...},{...},...];
+* JSONArray.OrderBy('colname1') return [{...},{...},...];
+* JSONArray.OrderByDesc('colname2') return [{...},{...},...];
 * AND OR <> NOT = > < >= <=
 */
 (function () {
@@ -20,6 +24,7 @@
 
     // 编译后的缓存，提升效率
     var _cache = {};
+
     // 自定义运算符：JS运算符
     var _alias = [
         /@/g, "_e.",
@@ -35,10 +40,12 @@
     ];
     var _rQuote = /""/g;
     var _rQuoteTemp = /!~/g;
+
     // 编译自定义语法
     var _complite = function (code) {
         return eval("0," + code);
     }
+
     // 将自定义语法转成JS语法
     var _interpret = function (exp) {
         exp = exp.replace(_rQuote, "!~");
@@ -60,8 +67,9 @@
         }
         return arr.join('"');
     }
+
     // 定义模函数
-    var _templ = function (_list) {
+    var _queryTempl = function (_list) {
         var _ret = [];
         var _i = -1;
         for (var _k in _list) {
@@ -75,17 +83,33 @@
         return _ret;
     }
     .toString();
+
+    // 查找元素模函数
+    var _indexOfTempl = function (_list) {
+        for (var _k in _list) {
+            var _e = _list[_k];
+            if (_e != _proto[_k]) {
+                if ($C) {
+                    return _k;
+                }
+            }
+        }
+        return -1;
+    }
+    .toString();
+
     // 扩展查询的方法
     _proto.Query = function (exp) {
+        var pr = "Q";
         if (!exp) {
             return [];
         }
-        var fn = _cache[exp];
+        var fn = _cache[pr + exp];
         try {
             if (!fn) {
                 var code = _interpret(exp);
-                code = _templ.replace("$C", code);
-                fn = _cache[exp] = _complite(code);
+                code = _queryTempl.replace("$C", code);
+                fn = _cache[pr + exp] = _complite(code);
             }
             return fn(this);
         } catch (e) {
@@ -93,21 +117,54 @@
         }
     }
 
+    // 查找元素下标
+    _proto.IndexOf = function (exp) {
+        //debugger
+        var pr = "I";
+        if (!exp) {
+            return [];
+        }
+        var fn = _cache[pr + exp];
+        try {
+            if (!fn) {
+                var code = _interpret(exp);
+                code = _indexOfTempl.replace("$C", code);
+                fn = _cache[pr + exp] = _complite(code);
+            }
+            return fn(this);
+        } catch (e) {
+            return -1;
+        }
+    }
+
+    // 删除元素
+    _proto.Remove = function (exp) {
+        var res = [];
+        var index = -1;
+        do {
+            index = this.IndexOf(exp);
+            if (index > -1) {
+                res.push(this.splice(index, 1));
+            }
+        } while (index > -1)
+        return res;
+    }
+
     // 正序排序
     _proto.OrderBy = function (proname) {
-        return _objArrSort(this, proname);
+        return objArrSort(this, proname);
     }
 
     // 倒序排序
     _proto.OrderByDesc = function (proname) {
-        return _objArrSort(this, proname).reverse();
+        return objArrSort(this, proname).reverse();
     }
 
     // 对象数组排序  proname:"排序的字段"
-    function _objArrSort(objArr, proname) {
-        if (!objArr || objArr.length == 0)
+    function objArrSort(objArr, proname) {
+        if (!objArr || objArr.length === 0)
             return [];
-        if (!proname || proname == "")
+        if (!proname)
             return objArr;
 
         var tmpObj = {};
